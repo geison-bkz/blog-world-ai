754ac43d3c69f492ad7cbb001c4a4a583f0008d3
geison-bkz <geison_bf@hotmail.com>
2024-05-27 19:15:08 -0300

    Atualizado about us

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -16,62 +16,22 @@
 
         <div class="row featurette">
             <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading fw-normal lh-1">
-                    Oh yeah, it’s that good.
-                    <span class="text-body-secondary">See for yourself.</span>
-                </h2>
-                <p class="lead">
-                    Another featurette? Of course. More placeholder content here to give you an idea
-                    of how this layout would work with some actual real-world content in place.
-                </p>
+                <h2 class="featurette-heading fw-normal lh-1">Gustavo Rodrigues Lopes</h2>
+                <span class="text-body-secondary">gustavo.rlopes22@gmail.com</span>
+                <p class="lead">Engenharia Elétrica (7° Semestre)</p>
             </div>
-            <div class="col-md-5 order-md-1">
-                <svg
-                    class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-                    width="500"
-                    height="500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-label="Placeholder: 500x500"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
-                    <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
-                </svg>
-            </div>
+            <div class="col-md-5 order-md-1 banner-img gustavo"></div>
         </div>
 
         <hr class="featurette-divider" />
 
         <div class="row featurette">
             <div class="col-md-7">
-                <h2 class="featurette-heading fw-normal lh-1">
-                    And lastly, this one. <span class="text-body-secondary">Checkmate.</span>
-                </h2>
-                <p class="lead">
-                    And yes, this is the last block of representative placeholder content. Again,
-                    not really intended to be actually read, simply here to give you a better view
-                    of what this would look like with some actual content. Your content.
-                </p>
+                <h2 class="featurette-heading fw-normal lh-1">Letícia Gonçalves Bardella</h2>
+                <span class="text-body-secondary">leticiabardella.7890@aluno.saojudas.br</span>
+                <p class="lead">engenharia elétrica (7° semestre)</p>
             </div>
-            <div class="col-md-5">
-                <svg
-                    class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-                    width="500"
-                    height="500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-label="Placeholder: 500x500"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
-                    <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text>
-                </svg>
-            </div>
+            <div class="col-md-5 banner-img leticia"></div>
         </div>
 
         <hr class="featurette-divider" />
@@ -90,5 +50,13 @@
 
 .vitor {
     background-image: url(../assets/images/profiles/vitor-profile.jpg);
+}
+
+.gustavo {
+    background-image: url(../assets/images/profiles/gustavo-profile.jpg);
+}
+
+.leticia {
+    background-image: url(../assets/images/profiles/leticia-profile.jpg);
 }
 </style>
